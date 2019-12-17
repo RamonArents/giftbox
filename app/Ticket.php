8d63b46daac5b29ff968f777dbf54af842b94b76
RamonArents <ramonarents@hotmail.com>
2019-12-17 10:51:49 +0100

    encryption deleted

--- app/Ticket.php
+++ app/Ticket.php
@@ -3,23 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use GregoryDuckworth\Encryptable\EncryptableTrait;
 
 
 class Ticket extends Model
 {
-    //The ticket codes should be encrypted, otherwise anyone can use them
-    use EncryptableTrait;
 
     protected $table = 'tickets';
-
-    /**
-     * Encryptable Rules
-     *
-     * @var array
-     */
-    protected $encryptable = [
-        'orderNumber',
-    ];
 
 }
