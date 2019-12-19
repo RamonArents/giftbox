64038f53a67803b86ebc48948f245d8ab7d952e2
RamonArents <ramonarents@hotmail.com>
2019-12-19 17:54:15 +0100

    email styled

--- resources/views/mail/mail.blade.php
+++ resources/views/mail/mail.blade.php
@@ -6,9 +6,10 @@
     <style>
         body{
             font-family: "Nunito", sans-serif;
+            background:#333;
         }
         header{
-            background-color:#4d3a18;
+            background-color:saddlebrown;
             color:white;
             height: 125px;
             min-width: 100%;
@@ -19,18 +20,39 @@
         p{
             font-size:1em;
         }
+        footer{
+            background-color:saddlebrown;
+            color:white;
+            height: 80px;
+            min-width: 100%;
+            text-align: center;
+            font-size:1.5em;
+            padding-top:5px;
+        }
+        .mail-content{
+            background-color:#f1f1f1;
+            color:#2c3e50;
+            padding: 10px;
+            text-align: center;
+            height: 100%;
+            width:100%;
+        }
+
     </style>
 </head>
 <body>
-        <header>
-            <h1>Code om kaarsje op te steken.</h1>
-        </header>
-        <div class="text-center mail-content">
-            <p>Hierbij ontvangt u de code om een kaarsje op te steken. Deze kan worden verzilverd op de website en is voor eenmalig gebruik.</p>
-            <p>Code: {{  $ticketNumber }}</p>
-            <p>Het is niet mogelijk om op deze email te reageren. Bij problemen kunt u contact opnemen met .....</p>
-            <p>Met vriendelijke groet,</p>
-            <p>Giftbox.</p>
-    </div>
+<header>
+    <h1>Code om kaarsje op te steken.</h1>
+</header>
+<div class="mail-content">
+    <p>Hierbij ontvangt u de code om een kaarsje op te steken. Deze kan worden verzilverd op de website en is voor eenmalig gebruik.</p>
+    <p>Code: {{  $ticketNumber }}</p>
+    <p>Het is niet mogelijk om op deze email te reageren. Bij problemen kunt u contact opnemen met (plaats contactgegevens hier).</p>
+    <p>Met vriendelijke groet,</p>
+    <p>Giftbox.</p>
+</div>
+<footer>
+    <p>Copyright Giftbox <script>let date = new Date(); document.write(date.getFullYear());</script></p>
+</footer>
 </body>
 </html>
