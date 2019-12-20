fec61c24b5ede29b08b93f019832228c9a4d2b79
RamonArents <ramonarents@hotmail.com>
2019-12-20 15:13:23 +0100

    styling donatiepage

--- resources/views/doneer.blade.php
+++ resources/views/doneer.blade.php
@@ -8,24 +8,18 @@
                     src="https://player.twitch.tv/?channel=giftbox_hsleiden"
                     frameborder="0"
                     allowfullscreen="true"
-                    scrolling="no"
-                    height="378"
-                    width="620"></iframe>
-            <div class="doneer-button">
+                    scrolling="no"></iframe>
                 <form action="{{ action('UserPageController@useCode') }}" method="post">
                     {{ csrf_field() }}
-                    <div class="form-group buy-ticket">
+                    <div class="form-group col-xl-3 mx-auto">
                         <label for="code">Verzilver code:</label><br>
                         <input class="form-control" id="code" type="text" name="code" required/>
                     </div>
                     <input class="doneer-buttons" type="submit" value="Steek kaarsje aan.">
                 </form>
-                <br>
-                <br>
+                <p>Of:</p>
                 <!--Buy ticket to use on website-->
                 <a class="btn doneer-buttons" href="{{ route('buycode') }}">Koop code</a>
-            </div>
-
         </div>
     </div>
 @endsection
