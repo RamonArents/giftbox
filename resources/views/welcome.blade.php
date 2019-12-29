dbad778067c518c1cd3e26485de6b8ee796216bd
RamonArents <ramonarents@hotmail.com>
2019-12-29 12:30:28 +0100

    sytling homepage changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,6 @@
     body{
         overflow: hidden;
     }
-    @media (max-width: 1100px){
-        body{
-            overflow: visible;
-        }
-    }
 </style>
 @section('content')
     <!--This is the background image-->
@@ -18,11 +13,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-5">
-            <div class="content">
-                <p class="homepage-text">Op deze website kunt u een kaarsje opsteken voor uw dierbare. Door een donatie te doen aan de kerk krijgt u een code. Met deze code kunt u uw kaarsje live zien gaan branden.</p>
-                <div class="homepage-button">
-                    <a href="{{ route('donatiepage') }}" class="btn">Doneer</a>
-                </div>
+            <div class="text-center homepageview">
+                <p>Op deze website kunt u een kaarsje opsteken voor uw dierbare. Door een donatie te doen aan de kerk krijgt u een code. Met deze code kunt u uw kaarsje live zien gaan branden.</p>
+                <a href="{{ route('donatiepage') }}" class="btn homepage-button">Doneer</a>
             </div>
         </div>
         </div>
